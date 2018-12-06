7dd54295200828b7c018575d24640d3fe5889fd8
Annie Gilchrist <anneliese.gilchrist@gmail.com>
2018-12-06 18:37:36 +0100

    finishing js and layout on feed

--- app/javascript/components/modals.js
+++ app/javascript/components/modals.js
@@ -1,10 +1,3 @@
-// function() {
-//   var modal_button = document.querySelectorAll("modal-button");
-//   if (modal_button) {
-//     modal_button.addEventListener("change", updateImageDisplay);
-//   }
-// }
-
 export default function() {
   $('#myModal').on('show.bs.modal', function (event ) {
     var button = $(event.relatedTarget)
@@ -12,14 +5,15 @@ export default function() {
     var charity = button.data('charity')
     var location = button.data('location')
     var description = button.data('description')
+    var charityPath = button.data('charityPath')
+    var applyPath = button.data('applyPath')
     var modal = $(this)
     modal.find('.modal-title').text("-" + name + "-")
     modal.find('.modal-info .charity').text(charity)
     modal.find('.modal-info .location').text(location)
-
-    // var button = $(event.relatedTarget)
-    // var name = button.data('name')
+    modal.find('.modal-body').text(description)
+    $('.visit-charity-button').attr('href', charityPath)
+    $('.apply-button').attr('href', applyPath)
   });
 }
 
-// charity location description
