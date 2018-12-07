b1cd1477a2ef678fcc146ed138a67f9a38a45437
Annie Gilchrist <anneliese.gilchrist@gmail.com>
2018-12-07 09:46:16 +0100

    tweaks to follow mechanism

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -4,6 +4,7 @@ class Charity < ApplicationRecord
   validates :description, presence: true, allow_blank: false
   has_many :users, as: :organisation, dependent: :destroy
   has_many :missions, dependent: :destroy
+  has_many :follows, dependent: :destroy
 
   mount_uploader :logo, PhotoUploader
 
