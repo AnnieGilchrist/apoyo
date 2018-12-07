6a1f2f08e02ecb9921dfe48b245a1dbe1330dc36
Ian Hunter <hunteia@gmail.com>
2018-12-07 13:33:44 +0100

    Seed work through cloudinary

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,4 +2,6 @@ class Mission < ApplicationRecord
   validates :name, presence: true, allow_blank: false
   validates :description, presence: true
   belongs_to :charity
+
+  mount_uploader :image, PhotoUploader
 end
