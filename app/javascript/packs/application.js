cf940a0b6bd0eb590c2b7c892520ed8ffcdbb926
Annie Gilchrist <anneliese.gilchrist@gmail.com>
2018-12-13 15:27:00 +0100

    messaging css and bugs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,9 @@ import $ from 'jquery';
 import profileUploader from '../components/image_upload';
 import filter from '../components/browse_filter';
 import modal from '../components/modals';
+import scroll from '../components/scroll';
 
 profileUploader()
 filter()
 modal()
+scroll()
